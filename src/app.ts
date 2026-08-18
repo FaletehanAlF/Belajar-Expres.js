75cbb178720a78ad18a99b766d68536264a6b1b0
FaletehanAlF <faletehanalfarabi09>
2026-08-18 13:51:58 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,87 @@
+import express, { type Express, type Request, type Response } from 'express';
+import cors from "cors"
+import { title } from 'node:process';
+
+const app: Express = express();
+const port = 8000;
+
+app.use(cors ())
+
+const sepatu = [
+  {
+    title: "Nike AiR F1",
+    category: "Kets"
+  },
+   {
+    title: "Puma Speedcat",
+     category: "Kets"
+  },
+   {
+    title: "Adidas Samba",
+    category: "Kets"
+
+  },
+]
+
+
+const buah = [
+  {
+    title: "Jeruk",
+    color: "orange"
+  },
+   {
+    title: "apel",
+     color: "merah"
+  },
+   {
+    title: "melon",
+    color: "hijau"
+
+  },
+]
+app.get("/api/sepatu", (req: Request,res: Response) => {
+  res.status(200).json({
+  message: "Berhasil fetch data sepatu!",
+  data: sepatu
+  })
+ 
+})
+
+app.get("/api/buah", (req: Request,res: Response) => {
+  res.status(200).json({
+  message: "Berhasil fetch data sepatu!",
+  data: sepatu
+  })
+ 
+})
+
+// app.get('/api/products', (req: Request, res: Response) => {
+//     res.status(200).json({
+//         message: "berhasil di tampilkan",
+//         data: [{
+//             name: "sepatu",
+//             category: "kets",
+//             price: 1500000
+//         }]
+//     })
+// });
+
+// app.post("/api/sepatu", (req, res) => {
+//   res.status(201).json({
+//     message: "Product berhasil ditambahkan"
+//   });
+// });
+// app.put("/api/sepatu", (req, res) => {
+//   res.status(200).json({
+//     message: `Product berhasil di update`
+//   });
+// });
+// app.delete("/api/sepatu", (req, res) => {
+//   res.status(204).json({
+//     message: `Product berhasil dihapus`
+//   });
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
